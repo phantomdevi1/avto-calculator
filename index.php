f7df2998666f44b639d00b27ce2cbb7a4b87cd8c
phantomdevi1 <gruzdev_ilya16@mail.ru>
2025-10-04 18:05:13 +0300

    new design

--- index.php
+++ index.php
@@ -9,20 +9,39 @@ if (!isset($_SESSION['user'])) {
 <html lang="ru">
 <head>
   <meta charset="UTF-8">
-  <title>Главная</title>
-<link rel='shortcut icon' href='img/favicon.png' type='image/x-icon'>
+  <title>Главная — Финансовые расчёты TANK</title>
+  <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div class="container">
+    <img src="img/logo.png" alt="" class="index_logo">
     <h2>Добро пожаловать, <?= htmlspecialchars($_SESSION['user']) ?>!</h2>
-    <p class="security_status_text">Это защищённая страница.</p>
-    <div class="container_home_href">
-      <a href="osago.php" class="block_href">Расчёт ОСАГО</a>
-      <a href="kasko.php" class="block_href">Расчёт КАСКО</a>
-      <a href="credit.php" class="block_href">Расчёт автокредита</a>
-      <a href="tradein.php" class="block_href">Расчёт TRADE-IN</a>
+    <p class="security_status_text">Это защищённая страница личного кабинета.</p>
+
+    <div class="container_home_href">      
+      <a href="osago.php" class="block_href">
+        <img src="img/osago_icon.png" alt="ОСАГО">
+        <span>Расчёт ОСАГО</span>
+      </a>
+
+      <a href="kasko.php" class="block_href">
+        <img src="img/casco_icon.png" alt="КАСКО">
+        <span>Расчёт КАСКО</span>
+      </a>
+
+      <a href="credit.php" class="block_href">
+        <img src="img/credit_icon.png" alt="Кредит">
+        <span>Расчёт автокредита</span>
+      </a>
+
+      <a href="tradein.php" class="block_href">
+        <img src="img/tradein_icon.png" alt="TRADE-IN">
+        <span>Расчёт TRADE-IN</span>
+      </a>
+
     </div>
+
     <a class="logout_btn" href="logout.php">Выйти</a>
   </div>
 </body>
